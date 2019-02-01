4826deb89499c9a68db2d72326b9c04639f33039
emreyalvac <emre.stress@gmail.com>
2019-02-01 12:05:11 +0300

    dynamic OID

--- server-managment-system/Controllers/snmpController.cs
+++ server-managment-system/Controllers/snmpController.cs
@@ -8,6 +8,8 @@ using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Mvc;
 using Lextm.SharpSnmpLib;
 using Lextm.SharpSnmpLib.Messaging;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json;
 using server_managment_system.Repostories.Cpu;
 
 namespace server_managment_system.Controllers
@@ -24,15 +26,19 @@ namespace server_managment_system.Controllers
         }
 
         [HttpGet, Route("snmp")]
-        public JsonResult WMI()
+        public string WMI([FromQuery] string identifier)
         {
             ISnmpData data = null,
-                    data2 = null;
+                data2 = null;
             GetNextRequestMessage message = new GetNextRequestMessage(0,
                 VersionCode.V1,
                 new OctetString("public"),
-                new List<Variable> {new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.5"))});
+                //new List<Variable> {new Variable(new ObjectIdentifier("1.3.6.1.2.1.25.1.6"))});
+                new List<Variable> {new Variable(new ObjectIdentifier(identifier))});
             ISnmpMessage response = message.GetResponse(60000, new IPEndPoint(IPAddress.Any, 161));
+
+            string s = JsonConvert.SerializeObject(response.Pdu().Variables);
+
             foreach (var a in response.Pdu().Variables)
             {
                 data2 = a.Data;
@@ -43,15 +49,27 @@ namespace server_managment_system.Controllers
                 new OctetString("public"),
                 new List<Variable> {new Variable(new ObjectIdentifier(".1.3.6.1.2.1.1.3"))},
                 60000);
+            s += JsonConvert.SerializeObject(result);
+            return s;
+        }
+
+        public JsonResult getSingleInformation()
+        {
+            ISnmpData data = null;
+            var result = Messenger.Get(VersionCode.V2,
+                new IPEndPoint(IPAddress.Any, 161),
+                new OctetString("public"),
+                new List<Variable> {new Variable(new ObjectIdentifier(".1.3.6.1.2.1.1.3"))},
+                60000);
             foreach (var a in result)
             {
                 data = a.Data;
             }
 
+
             return new JsonResult(new
             {
                 SystemUpTime = data.ToString(),
-                other = data2.ToString()
             });
         }
     }
